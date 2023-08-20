f9aa01ebfff5da5fc98009a02b91c40182806000
rubayet-islam <season.ss2001@gmail.com>
2023-08-20 22:52:12 +0600

    building module route, controller, service, validation, constants, interface

---
+++ src/app/modules/building/building.route.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { BuildingController } from './building.controller';
+import { BuildingValidation } from './building.validation';
+
+const router = express.Router();
+
+router.get('/', BuildingController.getAllBuildings);
+router.get('/:id', BuildingController.getSingleBuilding);
+
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(BuildingValidation.createZodBuilding),
+  BuildingController.createBuilding
+);
+
+router.patch(
+  '/:id',
+  validateRequest(BuildingValidation.updateZodBuilding),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  BuildingController.updateBuilding
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  BuildingController.deleteBuilding
+);
+
+export const BuildingRoutes = router;
