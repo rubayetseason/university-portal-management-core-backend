01747673c57837a8b6ebde4c75b91fcd1781f66e
rubayet-islam <season.ss2001@gmail.com>
2023-09-06 17:58:47 +0600

    regex validation added to zodvalidation

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.route.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.route.ts
@@ -3,6 +3,7 @@ import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { OfferedCourseClassScheduleController } from './offeredCourseClassSchedule.controller';
+import { OfferedCourseClassScheduleValidation } from './offeredCourseClassSchedule.validation';
 
 const router = express.Router();
 
@@ -11,14 +12,14 @@ router.get('/:id', OfferedCourseClassScheduleController.getSingleClassSchedule);
 
 router.post(
   '/',
-  validateRequest(OfferedCourseClassScheduleValidation.create),
+  validateRequest(OfferedCourseClassScheduleValidation.createZodClassSchedule),
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   OfferedCourseClassScheduleController.createClassSchedule
 );
 
 router.patch(
   '/:id',
-  validateRequest(OfferedCourseClassScheduleValidation.update),
+  validateRequest(OfferedCourseClassScheduleValidation.updateZodClassSchedule),
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   OfferedCourseClassScheduleController.updateClassSchedule
 );
